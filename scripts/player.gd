a120c9c8d685f2665dbb24ed2ba864c7fe5ee69a
SmartCat <colab0516@gmail.com>
2026-04-13 21:25:25 +0800

    Auto-sync by SmartCat at 21:25:22

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
-@export var speed = 400.0
+const SPEED = 200.0
 func _physics_process(_delta):
     var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-    velocity = dir * speed
+    velocity = dir * SPEED
+    if dir.x != 0: $KnightAnim.flip_h = dir.x < 0
     move_and_slide()
