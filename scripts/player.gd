0db3e7bb0964f51908dd6e894ab315f66b76cbd0
SmartCat <colab0516@gmail.com>
2026-04-13 16:07:24 +0800

    Auto-sync by SmartCat at 16:07:23

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,22 @@
 extends CharacterBody2D
 @export var speed = 300.0
+@export var jump_p = 60.0
+var is_jumping = false
 @onready var anim = $KnightAnim
-func _physics_process(delta):
-    var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-    if input_dir != Vector2.ZERO:
-        velocity = input_dir * speed
-        anim.flip_h = (input_dir.x < 0)
+
+func _physics_process(_delta):
+    var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+    if dir != Vector2.ZERO:
+        velocity = dir * speed
+        if dir.x != 0: anim.flip_h = dir.x < 0
         anim.play("default")
     else:
         velocity = velocity.move_toward(Vector2.ZERO, speed)
-        anim.stop()
+    
+    if Input.is_action_just_pressed("ui_accept") and not is_jumping:
+        is_jumping = true
+        var tw = create_tween()
+        tw.tween_property(anim, "offset:y", -jump_p, 0.2).set_trans(Tween.TRANS_SINE)
+        tw.tween_property(anim, "offset:y", 0, 0.2).set_trans(Tween.TRANS_SINE)
+        tw.finished.connect(func(): is_jumping = false)
     move_and_slide()
