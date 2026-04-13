9afd049e99768e8d28a600a125a2a86a489c37fd
SmartCat <colab0516@gmail.com>
2026-04-13 21:27:13 +0800

    Auto-sync by SmartCat at 21:27:10

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,5 +3,6 @@ const SPEED = 200.0
 func _physics_process(_delta):
     var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
     velocity = dir * SPEED
-    if dir.x != 0: $KnightAnim.flip_h = dir.x < 0
+    if dir.x != 0:
+        $KnightAnim.flip_h = dir.x < 0
     move_and_slide()
