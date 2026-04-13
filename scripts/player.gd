dca80bef14dae950b7293d5ce3eeada4dfdc9525
SmartCat <colab0516@gmail.com>
2026-04-13 20:53:56 +0800

    Auto-sync by SmartCat at 20:53:52

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
-@export var speed = 300.0
 func _physics_process(_delta):
     var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-    velocity = dir * speed
+    velocity = dir * 400.0
     move_and_slide()
